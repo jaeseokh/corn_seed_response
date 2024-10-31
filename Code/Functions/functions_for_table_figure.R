95f59cf4ac55627730d72fcc1a7c7261ce5b9669
jaeseok2illinois <jaeseok2@illinois.edu>
2024-10-30 20:11:34 -0400

    updated of writing_qmd, making_figure functions

--- Code/Functions/functions_for_table_figure.R
+++ Code/Functions/functions_for_table_figure.R
@@ -1,4 +1,22 @@
 
+# Define a function to create plots
+create_combined_plot <- function(data, item, y_label, color) {
+  ggplot(data %>% filter(Item == item)) +
+    geom_line(aes(x = Year, y = Value, linetype = "Unadjusted"), size = 1, color = color) +
+    geom_line(aes(x = Year, y = Value_adjusted, linetype = "PPI Adjusted"), size = 1, color = color) +
+    scale_linetype_manual(values = c("Unadjusted" = "dashed", "PPI Adjusted" = "solid")) +
+    labs(y = y_label, linetype = "") +
+    scale_x_continuous(breaks = seq(1996, 2024, by = 4), limits = c(1996, 2024)) +
+    theme_minimal() +
+     theme(
+      legend.position= "none",
+         axis.title.x = element_blank(),
+      axis.title = element_text(size = 18),       # Increase axis title font size
+      axis.text = element_text(size = 16),        # Increase axis text font size
+      legend.text = element_text(size = 16)       # Increase legend font size
+    )
+}
+
 
 process_info_table <- function(input_tb_binded) {
   # Step 2: Calculate the quadratic fit and yield maximizing seeding rate for each field
@@ -21,7 +39,7 @@ process_info_table <- function(input_tb_binded) {
     dif_s < 0 & quad_fit < 0 & ymsr == eosr, "A1",  # under_seed & corner sol
     dif_s < 0 & quad_fit < 0 & ymsr != eosr, "A2",
     dif_s > 0 & quad_fit < 0 & ymsr == eosr, "B1",  # over_seed & corner sol
-    dif_s > 0 & quad_fit < 0 & ymsr != eosr, "B2",  # over_seed & corner sol
+    dif_s > 0 & quad_fit < 0 & ymsr != eosr, "B2",  
     quad_fit > 0, "C",
     default = NA_character_  # Default value if no conditions match
   )]
